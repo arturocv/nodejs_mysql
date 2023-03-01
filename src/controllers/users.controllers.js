877edf82f373b70de7fba071055bab56e70f98f1
arturocv <kngarturo@gmail.com>
2023-02-28 23:08:58 -0500

    desinstalar bcryt
    desinstalar bcryt

--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -16,28 +16,44 @@ export const getLogin = async(req, res) => {
         const {email, password} = req.body;
 
         const [result] = await pool.query("SELECT * FROM usuarios WHERE email= ?", email);
-        
-        if(result.length === 0){
+
+        if( result.length === 0 ){
             return res.status(400).json({ msg: "User does not exist.", result });
         }else{
-            bcrypt.compare(password, result[0].password).then((esCorrecto) => {
-                if(esCorrecto){
-                    const { nombres, email, letra, texto, ruta } = result[0];                    
-                    res.status(200).json({
-                        msg: "Usuario logeado correctamente",
-                        usuario: {
-                            nombres,
-                            email,
-                            letra,
-                            texto,
-                            ruta                     
-                        },
-                    });                    
-                }else{
-                    return res.status(400).json({ msg: "User or password incorrect" });
-                }
-            })
+            const { nombres, email, letra, texto, ruta } = result[0];                    
+            res.status(200).json({
+                msg: "Usuario logeado correctamente",
+                usuario: {
+                    nombres,
+                    email,
+                    letra,
+                    texto,
+                    ruta                     
+                },
+            });
         }
+        
+        // if(result.length === 0){
+        //     return res.status(400).json({ msg: "User does not exist.", result });
+        // }else{
+        //     bcrypt.compare(password, result[0].password).then((esCorrecto) => {
+        //         if(esCorrecto){
+        //             const { nombres, email, letra, texto, ruta } = result[0];                    
+        //             res.status(200).json({
+        //                 msg: "Usuario logeado correctamente",
+        //                 usuario: {
+        //                     nombres,
+        //                     email,
+        //                     letra,
+        //                     texto,
+        //                     ruta                     
+        //                 },
+        //             });                    
+        //         }else{
+        //             return res.status(400).json({ msg: "User or password incorrect" });
+        //         }
+        //     })
+        // }
         
     } catch (error) {
         return res.status(500).json({ msg: "Something goes wrong", error });
